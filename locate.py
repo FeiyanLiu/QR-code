a27ddf18d813f870ed1b98833df1490412532d4d
XMUlinhui <53165595+XMUlinhui@users.noreply.github.com>
2020-03-16 08:57:04 +0800

    带注释版本定位识别

--- locate.py
+++ locate.py
@@ -12,57 +12,62 @@ import copy
 
 
 
-
+'''检测轮廓'''
 def detect(image):
 
     width,height=image.shape[:2][::-1]
-    img_gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
-    retval,binary=cv2.threshold(img_gray,0,255,cv2.THRESH_OTSU+cv2.THRESH_BINARY_INV)
-    contours,hierarchy=cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    img_gray=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)#转灰度图
+    retval,binary=cv2.threshold(img_gray,0,255,cv2.THRESH_OTSU+cv2.THRESH_BINARY_INV)#二值化处理
+    contours,hierarchy=cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)#等级树结构轮廓
     #cv2.drawContours(img,contours,-1,(0,0,255),3)
     #cv2.imshow("img",image)
-    cv2.waitKey()
+    #cv2.waitKey()
     return contours,hierarchy
-
+'''
+轮廓比例 边占比7：5
+'''
 def get_scale1(contours,i,j):
-#外轮廓和子轮廓比例
-    area1=cv2.contourArea(contours[i])
-    area2=cv2.contourArea(contours[j])
-    if area2==0:
+#外轮廓和子轮廓比例 1:1：3：1:1
+    area1=cv2.contourArea(contours[i])#轮廓面积1
+    area2=cv2.contourArea(contours[j])#轮廓面积2
+    if area2==0:#无子轮廓
         return False
     ratio = area1*1.0 / area2
-    if abs(ratio-49.0/25):
+    if abs(ratio-49.0/25): # 7/5
         return True
     return False
-
+'''轮廓比例 边占比5：3'''
 def get_scale2(contours,i,j):
     #子轮廓和子子轮廓
-    area1 = cv2.contourArea(contours[i])
-    area2 = cv2.contourArea(contours[j])
-    if area2 == 0:
+    area1 = cv2.contourArea(contours[i])#轮廓面积1
+    area2 = cv2.contourArea(contours[j])#轮廓面积2
+    if area2 == 0:#无子轮廓
         return False
     ratio = area1 * 1.0 / area2
-    if abs(ratio - 25.0 / 9):
+    if abs(ratio - 25.0 / 9):# 5/3
         return True
     return False
-
-def get_center(contours,i,):
-    M=cv2.moments(contours[i])
-    cx=int(M['m10']/M['m00'])
-    cy=int(M['m01']/M['m00'])
+'''
+求轮廓中心坐标
+'''
+def get_center(contours,i):
+    M=cv2.moments(contours[i]) #求矩阵
+    cx=int(M['m10']/M['m00']) #求x坐标
+    cy=int(M['m01']/M['m00']) #求y坐标
     return cx,cy
-
+'''判断中心间距'''
 def detect_contours(vec):
-    distance_1=np.sqrt((vec[0]-vec[2])**2+(vec[1]-vec[3])**2)
-    distance_2 = np.sqrt((vec[0] - vec[4]) ** 2 + (vec[1] - vec[5]) ** 2)
-    distance_3 = np.sqrt((vec[2] - vec[4]) ** 2 + (vec[3] - vec[5]) ** 2)
+    distance_1=np.sqrt((vec[0]-vec[2])**2+(vec[1]-vec[3])**2)#1、2两点的距离
+    distance_2 = np.sqrt((vec[0] - vec[4]) ** 2 + (vec[1] - vec[5]) ** 2)#1、3两点的距离
+    distance_3 = np.sqrt((vec[2] - vec[4]) ** 2 + (vec[3] - vec[5]) ** 2)#2、3两点的距离
     if sum((distance_1,distance_2,distance_3))/3<3:
         return True
     return False
 
-def triangle(rec):
+def triangle(rec):#是否组成三角形
     if len(rec) < 3:
         return -1, -1, -1
+    '''判断边长是否满足长方形条件'''
     for i in range(len(rec)):
         for j in range(i + 1, len(rec)):
             for k in range(j + 1, len(rec)):
@@ -79,55 +84,65 @@ def triangle(rec):
                     if abs(np.sqrt(np.square(distance_2) + np.square(distance_3)) - distance_1) < 5:
                         return i, j, k
     return -1, -1, -1
+'''
+用于寻找轮廓
+hierarchy[][i]，i 0-3 分别后一个轮廓的序号、前一个轮廓的序号、子轮廓的序号、父轮廓的序号
 
-def find(image,contours,hierachy,root=0):
+'''
+def find(image,contours,hierachy,root=0):#寻找轮廓
     rec=[]
     for i in range(len(hierachy)):
-        child = hierachy[i][2]
-        child_child=hierachy[child][2]
-        if child!=-1 and hierachy[child][2]!=-1:
+        child = hierachy[i][2]#得到子轮廓
+        child_child=hierachy[child][2]#得到子子轮廓
+        if child!=-1 and hierachy[child][2]!=-1:#两种轮廓都存在
             if get_scale1(contours,i,child) and get_scale2(contours,child,child_child):
-                cx1,cy1=get_center(contours,i)
-                cx2,cy2=get_center(contours,child)
-                cx3,cy3=get_center(contours,child_child)
+                cx1,cy1=get_center(contours,i)#父轮廓中心
+                cx2,cy2=get_center(contours,child)#子轮廓中心
+                cx3,cy3=get_center(contours,child_child)#子子轮廓中心
                 if detect_contours([cx1,cy1,cx2,cy2,cx3,cy3]):
                     rec.append([cx1,cy1,cx2,cy2,cx3,cy3,i,child,child_child])
     i,j,k=triangle(rec)
+    '''不能成三角形，结束函数'''
     if i==-1 or j==-1 or k==-1:
         return
-    ts=np.concatenate((contours[rec[i][6]],contours[rec[j][6]],contours[rec[k][6]]))
-    rect=cv2.minAreaRect(ts)
-    box=cv2.boxPoints(rect)
+    ts=np.concatenate((contours[rec[i][6]],contours[rec[j][6]],contours[rec[k][6]]))#矩阵拼接
+    rect=cv2.minAreaRect(ts)#最小外接矩形
+    box=cv2.boxPoints(rect)#矩形边缘
     box=np.int0(box)
-    result= copy.deepcopy(image)
+    #result= copy.deepcopy(image)
     #cv2.drawContours(result, [box], 0, (0, 0, 255), 2)
     #cv2.drawContours(image, contours, rec[i][6], (255, 0, 0), 2)
     #cv2.drawContours(image, contours, rec[j][6], (255, 0, 0), 2)
     #cv2.drawContours(image, contours, rec[k][6], (255, 0, 0), 2)
     #cv2.imshow('img', image)
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
     #cv2.imshow('img', result)
-    cv2.waitKey(0)
-    new_image = copy.deepcopy(image)
+    #cv2.waitKey(0)
+    #new_image = copy.deepcopy(image)#复制备份
 
 
-    leftup=box[1][1]
-    rightup=box[0][1]
-    leftbuttom=box[0][0]
-    rightbuttom=box[2][0]
+    #leftup=box[1][1]
+    #rightup=box[0][1]
+    #leftbuttom=box[0][0]
+    #rightbuttom=box[2][0]
+    '''
+    测试出的位置
+    '''
     leftup = box[2][1]
     rightup = box[0][1]
     leftbuttom = box[1][0]
     rightbuttom = box[3][0]
 
-
+    '''
+    裁剪图片
+    '''
     new_image=image[leftup:rightup,leftbuttom:rightbuttom]
     #new_image=image[43:1025,403:1387]
-    cv2.imshow('img', new_image)
-    cv2.waitKey(0)
+    #cv2.imshow('img', new_image)
+    #cv2.waitKey(0)
     new_image1=cv2.resize(new_image,(512,512))
-    cv2.imshow('img', new_image1)
-    cv2.waitKey(0)
+    #cv2.imshow('img', new_image1)
+    #cv2.waitKey(0)
     return new_image1
 
 def bintostr(s):
@@ -146,11 +161,11 @@ def bintostr(s):
             outStr = outStr + (chr(sum))
             sum = 0
 
-    print(outStr)
+    #print(outStr)
     return outStr
 
 def decode(img):
-    cv2.imshow("img",img)
+    #cv2.imshow("img",img)
     contours, hierachy = detect(img)
     img=find(img, contours, np.squeeze(hierachy))
     if(type(img)==type(None)):
@@ -158,7 +173,7 @@ def decode(img):
         return
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY+cv2.THRESH_BINARY_INV)
-    cv2.imshow("img222",gray)
+    #cv2.imshow("img222",gray)
     cv2.waitKey()
     pic_number = 1
     size = 512
@@ -171,7 +186,7 @@ def decode(img):
     str1 = ""
     while countx < size:
         #print(np.sum(img[countx:countx + cube, county:county + cube]))
-        if np.sum(img[countx:countx + cube, county:county + cube]) < 70000:  # 这里相当于是取小像素块的平均值，考虑到后面手机拍摄可能会产生色差
+        if np.sum(img[countx:countx + cube, county:county + cube]) < 130000:  # 这里相当于是取小像素块的平均值，考虑到后面手机拍摄可能会产生色差
             bin1 = bin1 + '1'
         else:
             bin1 = bin1 + '0'
@@ -205,15 +220,19 @@ def decode(img):
     print(bin1)
     print(bintostr(bin1))
 
-
 #
 if __name__ == "__main__":
 
-    img_path = r'G:/project1pic/1.png'
+    #img_path = r'G:/project1pic/1.png'
     #img_path = r'G:/test1.jpg'
-    img_path = r'G:/test.jpg'
+    img_path = r'G:/testpic/test9.jpg'
+    img_path = r'G:/testpic/test1.png'
+    #img_path = r'G:/test.png'
     #img_path=r'G:/project1pic/1.png'
-    img=cv2.imread(img_path)
+    img = cv2.imread(img_path)
     #contours,hierachy=detect(img)
     #find(img,contours,np.squeeze(hierachy))
     decode(img)
+    # img_path = r'G:/testpic/test14.jpg'
+    # img=cv2.imread(img_path)
+    # decode(img)
